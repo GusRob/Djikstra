4083cd41b458ffbf5259ab511372fb99630790f1
Angus Robertson <angusprobertson@gmail.com>
2022-01-18 04:25:52 +0000

    basic fie parser for graph files

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,70 @@
 #include <DrawingWindow.h>
 #include <Colour.h>
 #include <fstream>
+#include <sstream>
 #include <vector>
+#include <Graph.h>
 
-#define WIDTH 700
-#define HEIGHT 700
+#define WIDTH 600
+#define HEIGHT 600
 
-std::string currentMap = "example";
+std::string currentMap = "";
+Graph map = Graph();
+
+//splits a string into a vector of integers
+std::vector<int> split(std::string in, char del){
+	std::vector<int> result;
+	std::string subString = "";
+	for(int i = 0; i < in.length()+1; i++){
+		if(in[i] == del){
+			result.push_back(std::stoi(subString));
+			subString = "";
+		} else {
+			subString.append(1, in[i]);
+		}
+	}
+	result.push_back(std::stoi(subString));
+	return result;
+}
 
 //updates the current map image and graph by reading the new file
 void updateMap(std::string newMap){
 	if(currentMap != newMap){
 		currentMap = newMap;
+		//open inputfilestream
+		std::ifstream ifs ("assets/" + newMap + "/" + newMap + "_graph.txt", std::ifstream::in);
+		//start reading chars
+		char c = ifs.get();
+		//reading mode
+		std::string reading = "locations";
+		//main read loop
+		std::string currentWord = "";
+		int point_count = 0;
+		int arc_count = 0;
+		while(ifs.good()){
+			if(c == '\n' || c == ' '){
+				//if graph read mode update
+				if(currentWord == "locations:"){
+					reading = "locations";
+				} else if(currentWord == "roads:"){
+					reading = "roads";
+				} else {
+					//split word with comma delimiter
+					std::vector<int> elements = split(currentWord, ',');
+					if(reading == "locations"){
+						map.points.push_back(Point(elements[0], elements[1], point_count++));
+					} else if(reading == "roads"){
+						map.arcs.push_back(Arc(elements[0], elements[1], arc_count++, bool(elements[2])));
+					}
+				}
+				//
+				currentWord = "";
+			} else {
+				currentWord.append(1, c);
+			}
+			c = ifs.get();
+		}
+		ifs.close();
 	}
 }
 
@@ -34,19 +87,9 @@ void drawCircle(DrawingWindow &window, int cx, int cy, int r){
 //main draw function for page refreshes
 void draw(DrawingWindow &window) {
 	window.clearPixels();
-	/*
-	for (size_t y = 0; y < window.height; y++) {
-		for (size_t x = 0; x < window.width; x++) {
-			float red = (rand() % 256);
-			float green = (rand() % 256);
-			float blue = (rand() % 256);
-			uint32_t colour = (255 << 24) + (int(red) << 16) + (int(green) << 8) + int(blue);
-			window.setPixelColour(x, y, colour);
-		}
+	for(int i = 0; i < map.points.size(); i++){
+		drawCircle(window, map.points[i].x, map.points[i].y, 5);
 	}
-	*/
-
-	drawCircle(window, 100, 100, 5);
 }
 
 //event handler, after initial cleanse for quit function
@@ -66,6 +109,7 @@ void handleEvent(SDL_Event event, DrawingWindow &window) {
 int main(int argc, char *argv[]) {
 	DrawingWindow window = DrawingWindow(WIDTH, HEIGHT, false);
 	SDL_Event event;
+	updateMap("example");
 	while (true) {
 		// We MUST poll for events - otherwise the window will freeze !
 		if (window.pollForInputEvents(event)) handleEvent(event, window);
